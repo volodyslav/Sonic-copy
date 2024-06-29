95fa44701d7b8a4c62161caaeca8c4c682c00da5
volodyslav <vladver1211@gmail.com>
2024-06-29 20:57:42 +0300

    add: cloud and palms imags

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from settings import *
 from sprites import *
-
+from random import choice
 
 class Game:
     def __init__(self):
@@ -15,31 +15,48 @@ class Game:
 
         # Groups
         self.group_sprites = pygame.sprite.Group()
+        self.objects_group = pygame.sprite.Group()
 
         self.player = Player((PLAYER_POSITION_X, PLAYER_POSITION_Y), self.group_sprites)
-
         # Dt
         self.clock = pygame.time.Clock()
+
+        # Custom events
+        self.cloud_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.cloud_event, choice([300, 2000]))
+
+        self.trees_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.trees_event, choice([800, 1000, 1500]))
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == self.cloud_event:
+                    Clouds(self.objects_group)
+                if event.type == self.trees_event:
+                   Trees(self.objects_group)
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         sys.exit()
+
             self.show_screen()
 
     def show_screen(self):
-        self.clock.tick() / 1000
+        dt = self.clock.tick() / 1000
 
         # Fill the screen
         self.screen.fill(SCREEN_COLOR)
         # Draw the ground
         self.screen.blit(self.ground_image, self.ground_rect)
+        # dRAW CLOUDS
+        self.objects_group.draw(self.screen)
+        self.objects_group.update(dt)
+
         # Draw the group sprite
         self.group_sprites.draw(self.screen)
+        self.group_sprites.update(dt)
 
         pygame.display.flip()
 
--- sprites.py
+++ sprites.py
@@ -1,3 +1,5 @@
+import pygame
+from random import choice
 from settings import *
 
 
@@ -5,8 +7,57 @@ class Player(pygame.sprite.Sprite):
     """T-rex sprite"""
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load(join("images", "Player", "stand", "0.gif")).convert_alpha()
+        self.image = pygame.image.load(join("images", "player", "stand", "0.gif")).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
 
         self.frames_stand = []
         self.frames_stand_index = 0
+        self.load_images_stand()
+
+    def load_images_stand(self):
+        """Loads standing images"""
+        for image_path, sub_folder, image_name in walk(join("images", "player", "stand")):
+            if image_name:
+                for image_num in sorted(image_name, key=lambda n: int(n.split(".")[0])):
+                    full_path = join(image_path, image_num)
+                    surf = pygame.image.load(full_path)
+                    self.frames_stand.append(surf)
+
+    def move_stand(self, dt):
+        self.frames_stand_index = self.frames_stand_index + 5 * dt
+        self.image = self.frames_stand[int(self.frames_stand_index) % len(self.frames_stand)]
+
+    def update(self, dt):
+        self.move_stand(dt)
+
+
+class Clouds(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("images", "player", "cloud.png")).convert_alpha()
+        pos_x, pos_y = SCREEN_WIDTH + 100, choice([SCREEN_HEIGHT / 2, 100, 200, 300])
+        self.rect = self.image.get_frect(center=(pos_x, pos_y))
+        self.speed = 2
+
+    def update(self, dt):
+        """Moves"""
+        self.rect.x += -(self.speed + dt)
+        if self.rect.x < -200:
+            self.kill()
+
+
+class Trees(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.images = [pygame.image.load(join("images", "player", "palm2-min.png")).convert_alpha(),
+                       pygame.image.load(join("images", "player", "palm-min.png")).convert_alpha()]
+        self.image = choice(self.images)
+        pos_x, pos_y = SCREEN_WIDTH + 100, SCREEN_HEIGHT - 200
+        self.rect = self.image.get_frect(bottomright=(pos_x, pos_y))
+        self.speed = 2
+
+    def update(self, dt):
+        """Moves"""
+        self.rect.x += -(self.speed + dt)
+        if self.rect.x < -200:
+            self.kill()
